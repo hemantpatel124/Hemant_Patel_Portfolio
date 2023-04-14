0b4916b220422dac796b415c8d577f7715d27b31
hemant07 <hemantpatel124@gmail.com>
2023-04-14 15:11:58 +0530

    certificate updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,14 +3,18 @@
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll=()=>{
-    section.forEach(sec =>{
-        let top=window.scrollY;
-        let offset= sec.offsetTop -150;
-        let height =sec.offsetHeight;
-        let id =sec.getAttribute('id');
+window.onscroll = () => {
 
-        if(top >= offset && top < offset + height){
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -18,8 +22,7 @@ window.onscroll=()=>{
         };
     });
 
-//sticky navbar
-let header =document.querySelector('.header');
-
-header.classList.toggle('sticky',window.scrollY>100);
+    //sticky navbar
+    // let header = document.querySelector('.header');
+    // header.classList.toggle('sticky', window.scrollY > 100);
 };
