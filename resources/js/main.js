36e2ef86a93ed08906a65ff1141775659d779cc9
hemant07 <hemantpatel124@gmail.com>
2023-04-17 01:43:52 +0530

    portfolio template ready

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,21 @@
+//menu icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
 //scroll section active link
 
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-
-    let header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
     section.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -22,7 +30,59 @@ window.onscroll = () => {
         };
     });
 
-    //sticky navbar
-    // let header = document.querySelector('.header');
-    // header.classList.toggle('sticky', window.scrollY > 100);
+
+    //Sticky Navigation
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    //remove menu icon navbar when clicking on
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
 };
+
+
+
+
+
+//Swiper
+/*var swiper = new Swiper(".mySwiper",{
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});*/
+
+
+//dark icon mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+
+
+
+//scroll reveal mode
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 3000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .skill-container, .project-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
